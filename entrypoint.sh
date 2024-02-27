826385de0d7f0267f7f5a2ea04502c23775d0012
Frodo Podschwadek <frodo.podschwadek@gmail.com>
2024-02-27 11:56:06 +0000

    Update entrypoint script for handling custom group names in dynamic inventory.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,18 @@
 #!/bin/ash
+
+# Read inventory group from environment variable if available and replace
+# the default group name 'kittens' in the dynamic inventory file.
+# This is important for running serveral CATPOD-handled instances on the
+# same host, as we don't want to add containers from seperate Docker
+# applications to the same group.
+#
+# Check if CATPOD_INVENTORY_GROUP is set and not empty. If so, we use that as
+# custom group name.
+if [[ -n "$CATPOD_INVENTORY_GROUP" ]]; then
+    # Set custom group name in the dynamic inventory file.
+    sed -i "s/kittens/$CATPOD_INVENTORY_GROUP/g" /etc/ansible/docker.yml
+fi
+
 case "$1" in
     galaxy)
         shift
